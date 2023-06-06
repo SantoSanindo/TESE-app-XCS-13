a3450606b8d9d1db599c955b2a814c492451d359
SantoSanindo <86644267+SantoSanindo@users.noreply.github.com>
2023-06-06 07:11:44 +0700

    frmInterrupt

--- TESE app/frmInterrupt.Designer.vb
+++ TESE app/frmInterrupt.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmAlert
+Partial Class frmInterrupt
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,29 +23,42 @@ Partial Class frmAlert
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.IndianRed
-        Me.Label1.Location = New System.Drawing.Point(62, 46)
+        Me.Label1.Location = New System.Drawing.Point(40, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(351, 194)
-        Me.Label1.TabIndex = 0
+        Me.Label1.TabIndex = 1
         Me.Label1.Text = "Label1"
         '
-        'frmAlert
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(45, 217)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(346, 47)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Command 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmInterrupt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(482, 287)
+        Me.ClientSize = New System.Drawing.Size(435, 339)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "frmAlert"
-        Me.Text = "frmAlert"
+        Me.Name = "frmInterrupt"
+        Me.Text = "frmInterrupt"
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
